02afe22afc2da507b7e573c5980b179ceb8c6c99
Dazzle <henry@01power.net>
2025-08-19 10:13:25 +0100

    append2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Services from './pages/Services/Services';
@@ -24,6 +25,7 @@ function App() {
                 <Route path="/admin" element={<Admin />} />
               </Routes>
             </main>
+            <Footer />
           </div>
         </Router>
       </AppProvider>
